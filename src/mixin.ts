cca3fd061b06544839795777619ddcbc0718cb0c
Tomáš Klapka <tomas@klapka.cz>
2017-08-20 16:24:20 +0200

    mix interfaces in, add typings file (is it correct?)

--- src/mixin.ts
+++ src/mixin.ts
@@ -1,6 +1,7 @@
 import { getDebug, getFnDebug } from './debug';
 const debug = getDebug('mixin');
 
+import I = require('./rdf-interfaces');
 import { IProfile, IRDFEnvironment } from './rdf-interfaces';
 import { TermMap } from './term-map';
 import { PrefixMap } from './prefix-map';
@@ -43,12 +44,14 @@ function mixin(object?) {
     if (_extendeeName !== 'rdf-ext' && !object['defaults']) {
         object.defaults = newClasses;
     }
+    object._interfaces = I;
 
     // setup default environment
     object._defaultEnvironment = new RDFEnvironment(object);
     object._environment = object._defaultEnvironment;
 
     // get/set methods
+    object.interfaces = () => { return object._interfaces};
     object.extendeeName = (): string => { return object._extendeeName; }
     object.environment = (): IRDFEnvironment => { return object._environment; }
     object.setEnvironment = (env: IRDFEnvironment) => { object._environment = env; return object; }
